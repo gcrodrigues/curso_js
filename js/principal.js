7501e2506089d091c32032928a410c069fab0f67
Gustavo <gcrodrigues413@gmail.com>
2019-09-08 23:15:38 -0300

    Adicionando loop for para percorrer lista de pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,36 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso"); 
-var tdAltura = paciente.querySelector(".info-altura"); 
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for (var i =0 ; i <pacientes.length ;i++){
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso"); 
+    var tdAltura = paciente.querySelector(".info-altura"); 
 
-var pesoEhValido = true;
-var alturaEhValida = true;
-var tdImc = paciente.querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
-if(peso <=0 || peso >= 1000){
-    console.log("Peso inválido!");
-    tdImc.textContent = "Peso inválido";
-    pesoEhValido = false;
-}
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(altura<=0 || altura>=3){
-    console.log("Altura inválida!");
-    tdImc.textContent = "Altura inválida";
-    alturaEhValida = false;
-}
+    if(peso <=0 || peso >= 1000){
+        console.log("Peso inválido!");
+        tdImc.textContent = "Peso inválido";
+        pesoEhValido = false;
+    }
 
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc; 
+    if(altura<=0 || altura>=3){
+        console.log("Altura inválida!");
+        tdImc.textContent = "Altura inválida";
+        alturaEhValida = false;
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc; 
+    }
 }
 
