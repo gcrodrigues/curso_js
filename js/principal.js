af6bb5ff406e4e351a55c13c69bb3d94ca99f24f
Gustavo <gcrodrigues413@gmail.com>
2019-09-08 21:20:56 -0300

    Validando os dados recebidos na variável peso e altura

--- js/principal.js
+++ js/principal.js
@@ -8,6 +8,17 @@ var tdAltura = paciente.querySelector(".info-altura");
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
+
+if(peso<=0 || peso>=1000){
+    console.log("Peso inválido!");
+    tdImc.textContent = "Peso inválido";
+}
+
+if(altura<=0 || altura>=3){
+    console.log("Altura inválida!");
+    tdImc.textContent = "Altura inválida";
+}
+
 var imc = peso / (altura*altura);
 
 var tdImc = paciente.querySelector(".info-imc");
