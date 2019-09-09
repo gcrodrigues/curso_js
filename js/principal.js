84a473f86220c2d945e4c796254e223bc1d80395
Gustavo <gcrodrigues413@gmail.com>
2019-09-08 23:26:27 -0300

    Adicionando classe ao paciente com peso ou altura inválido

--- js/principal.js
+++ js/principal.js
@@ -20,12 +20,14 @@ for (var i =0 ; i <pacientes.length ;i++){
         console.log("Peso inválido!");
         tdImc.textContent = "Peso inválido";
         pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
     }
 
     if(altura<=0 || altura>=3){
         console.log("Altura inválida!");
         tdImc.textContent = "Altura inválida";
         alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
     }
 
     if(pesoEhValido && alturaEhValida){
