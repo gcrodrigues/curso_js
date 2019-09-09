51d2d919a078d798fbf6802ae66f5d9c1d409567
Gustavo <gcrodrigues413@gmail.com>
2019-09-08 21:16:02 -0300

    Realizando calculo do IMC e exibindo na tabela

--- js/principal.js
+++ js/principal.js
@@ -4,3 +4,11 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso"); 
 var tdAltura = paciente.querySelector(".info-altura"); 
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura*altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc; 
