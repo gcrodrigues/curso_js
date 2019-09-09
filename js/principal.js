81716f86f82c94c4346d33ae0ee09fdb63c3a025
Gustavo <gcrodrigues413@gmail.com>
2019-09-08 21:04:29 -0300

    Armazenando valores do peso e altura do paciente nas variaveis

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,5 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso"); 
+var tdAltura = paciente.querySelector(".info-altura"); 
