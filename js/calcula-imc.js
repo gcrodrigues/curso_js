2ef4863e6559271cc79e5011f25f31a36a9dd8b3
Gustavo <gcrodrigues413@gmail.com>
2019-10-01 19:44:18 -0300

    ; faltando na função calculaImc

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -39,7 +39,7 @@ for (var i=0 ; i <pacientes.length ;i++){
 function calculaImc(peso, altura){
     var imc = 0;
     imc = peso/(altura*altura);
-    return imc.toFixed(2)
+    return imc.toFixed(2);
 }
 
 
