588ea3fcf1f7298dd77301c9722dc11bd8915173
Gustavo <gcrodrigues413@gmail.com>
2019-10-01 17:59:59 -0300

    Tornando o nome do arquivo mais semântico

---
+++ js/calcula-imc.js
@@ -0,0 +1,40 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i=0 ; i <pacientes.length ;i++){
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso"); 
+    var tdAltura = paciente.querySelector(".info-altura"); 
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
+
+    if(peso <=0 || peso >= 1000){
+        console.log("Peso inválido!");
+        tdImc.textContent = "Peso inválido";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura<=0 || altura>=3){
+        console.log("Altura inválida!");
+        tdImc.textContent = "Altura inválida";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2); 
+    }
+}
+
+
+
